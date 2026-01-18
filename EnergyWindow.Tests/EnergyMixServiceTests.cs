d2738a72564c2dd10f6314b2b1b8b5e76ce02f2d
Zuzanna Cholewa <zuzanna.cholewa@gmail.com>
2026-01-18 13:33:23 +0100

    Add more tests

--- EnergyWindow.Tests/EnergyMixServiceTests.cs
+++ EnergyWindow.Tests/EnergyMixServiceTests.cs
@@ -64,4 +64,42 @@ public class EnergyMixServiceTests
         Assert.Equal(day2.Date, result[0].Date);
         Assert.Equal(80, result[0].CleanEnergyPercentage);
     }
+
+    [Fact]
+    public async Task GetOptimalWindowAsync_TooLargeHours_Throws()
+    {
+        var service = new EnergyMixService(null!);
+
+        await Assert.ThrowsAsync<ArgumentOutOfRangeException>(
+            () => service.GetOptimalWindowAsync(7));
+    }
+
+    [Fact]
+    public void BuildDailyMix_WhenOnlyOneDay_ReturnsEmpty()
+    {
+        var service = new EnergyMixService(null!);
+
+        var day1 = new DateTime(2024, 01, 10, 0, 0, 0, DateTimeKind.Utc);
+
+        var mix = new EnergyMix
+        {
+            Data =
+            {
+                new EnergyMix.Interval
+                {
+                    From = day1,
+                    To = day1.AddMinutes(30),
+                    GenerationMix =
+                    {
+                        new EnergyMix.GenerationMix { Fuel = "wind", Perc = 40 },
+                        new EnergyMix.GenerationMix { Fuel = "gas", Perc = 60 }
+                    }
+                }
+            }
+        };
+
+        var result = service.BuildDailyMix(mix);
+
+        Assert.Empty(result);
+    }
 }
--- obj/Debug/net9.0/EnergyWindow.AssemblyInfo.cs
+++ obj/Debug/net9.0/EnergyWindow.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EnergyWindow")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+512fa00d6b4527049b862d9aa6b27e1630f9f193")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c368ea460989d50de17fdcbde6a0f97720630ed0")]
 [assembly: System.Reflection.AssemblyProductAttribute("EnergyWindow")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EnergyWindow")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
